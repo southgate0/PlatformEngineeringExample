6df2209f53be2eed6895bd16b26b7dc904a7629f
David Southgate <d@davidsouthgate.co.uk>
2025-06-09 13:52:11 +0100

    New feature

--- PlatformEngineeringExample/ContentView.swift
+++ PlatformEngineeringExample/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, new feature!")
+            Text("Hello, new feature again!")
         }
         .padding()
     }
