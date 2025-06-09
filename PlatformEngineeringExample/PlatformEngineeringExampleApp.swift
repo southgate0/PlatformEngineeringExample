506f0ff652df60d1da7855cabf95dd85710633bf
David Southgate <d@davidsouthgate.co.uk>
2025-06-09 12:16:36 +0100

    Initial commit

---
+++ PlatformEngineeringExample/PlatformEngineeringExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatformEngineeringExampleApp.swift
+//  PlatformEngineeringExample
+//
+//  Created by Southgate on 09/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlatformEngineeringExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlatformEngineeringExampleTests/PlatformEngineeringExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlatformEngineeringExampleTests.swift
+//  PlatformEngineeringExampleTests
+//
+//  Created by Southgate on 09/06/2025.
+//
+
+import Testing
+
+struct PlatformEngineeringExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
